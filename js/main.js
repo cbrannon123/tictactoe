54f04cda9aaffa5bf1a4211cb7752e4a3191ea98
Casey Brannon <cbrannon412@gmail.com>
2019-04-30 18:43:05 -0700

    game

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 /*----- constants -----*/
 // Define required constants:
-  //1.1) Define a colors object with keys of 'null' (when the square is empty), and players 1 & -1. 
-  //The value assigned to each key represents the color to display for an empty square (null), player 1 and player -1.
 const colors = {
   '1': "red",
   '-1': "blue",
@@ -11,10 +9,8 @@ const colors = {
     
 
 };
-// every combo that could win
-  //1.2) Define the 8 possible winning combinations, 
-  //each containing three indexes of the board that make a winner if they hold the same player value.
-const Combos = [
+// winning combos
+  const Combos = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -27,9 +23,6 @@ const Combos = [
 
 
 /*----- app's state (variables) -----*/ 
-//Define required variables used to track the state of the game:
-//	2.1) Use a board array to represent the squares.	2.2) Use a turn variable to remember whose turn it is.
-//	2.3) Use a winner variable to represent three different possibilities - player that won, a tie, or game in play.
 var board;
 var turn;
 var winner;
@@ -46,14 +39,10 @@ document.querySelector('button').addEventListener('click', init);
 
 /*----- functions -----*/
 
-
-
-
 init()
 
-
+//finds sum of winning combos from 1 or -1 click, convert to + num with abs.Math
 function getWinner(){
-
 for(var i = 0; i < Combos.length; i++) {
    if (Math.abs(board[0] + board[1] + board[2]) === 3) return board[0]; 
    if (Math.abs(board[3] + board[4] + board[5]) === 3) return board[3];
@@ -63,8 +52,8 @@ for(var i = 0; i < Combos.length; i++) {
    if (Math.abs(board[2] + board[5] + board[8]) === 3) return board[2];
    if (Math.abs(board[0] + board[4] + board[8]) === 3) return board[0];
    if (Math.abs(board[2] + board[4] + board[6]) === 3) return board[2];
+   //if board is null after winner, return null to stop from selecting null squares 
    if (board.includes(null)) return null; 
-
     return 'T'
   }
 }
@@ -73,7 +62,7 @@ for(var i = 0; i < Combos.length; i++) {
 
   
   
-//targets the box
+
 function move(evt) {
   //takes the id and replaces the 'sq' with nothing and converst into number via parseInt
   var marker = evt.target;
